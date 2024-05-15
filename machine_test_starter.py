5e76df48b4adfa38f37efb4e84170da4952fd022
Kitio Myrian <kmyriantek@gmail.com>
2024-05-15 20:53:26 +0200

    fio test

--- machine_test_starter.py
+++ machine_test_starter.py
@@ -1,7 +1,5 @@
 import socket
 
-# To ping machines to start the test once deployment is done
-
 def build_machines_list_with_port(machines):
     output = []
     for machine in machines:
@@ -10,9 +8,13 @@ def build_machines_list_with_port(machines):
 
 def ping_machines_to_start(machines):
     peers = build_machines_list_with_port(machines)
-    # Send start message to each peer
     for ip, port in peers:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            sock.connect((ip, port))
-            sock.sendall("start".encode('utf-8'))
-            print(f"Sent start message to {ip}:{port}")
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                sock.connect((ip, port))
+                sock.sendall("start".encode('utf-8'))
+                print(f"Sent start message to {ip}:{port}")
+        except ConnectionRefusedError as e:
+            print(f"Connection to {ip}:{port} refused: {e}")
+        except socket.error as e:
+            print(f"Socket error occurred with {ip}:{port}: {e}")
